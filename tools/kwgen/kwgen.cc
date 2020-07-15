e6c950ce093aac6ebd35c6bde4f867bbaa9216b1
Roberto Raggi <roberto.raggi@gmail.com>
2020-07-15 16:58:02 +0000

    Reformat kwgen.cc
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- tools/kwgen/kwgen.cc
+++ tools/kwgen/kwgen.cc
@@ -1,34 +1,33 @@
 // Copyright (c) 2007 Roberto Raggi <roberto.raggi@gmail.com>
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-// ### TODO: Rewrite me.
-
-#include <string>
-#include <iostream>
-#include <map>
-#include <list>
-#include <vector>
-#include <set>
-#include <cstdlib>
-#include <cctype>
-#include <functional>
 #include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 class State;
 class DottedItem;
@@ -41,26 +40,21 @@ typedef std::string::iterator Dot;
 typedef std::vector<DottedItem>::iterator DottedItemPtr;
 
 class DottedItem {
-public:
+ public:
   RulePtr rule;
   Dot dot;
 
   DottedItem() {}
 
-  DottedItem(RulePtr rule, Dot dot):
-    rule(rule), dot(dot) {}
+  DottedItem(RulePtr rule, Dot dot) : rule(rule), dot(dot) {}
 
-  bool operator == (const DottedItem &other) const {
+  bool operator==(const DottedItem &other) const {
     return rule == other.rule && dot == other.dot;
   }
 
-  bool operator != (const DottedItem &other) const {
-    return ! operator == (other);
-  }
+  bool operator!=(const DottedItem &other) const { return !operator==(other); }
 
-  bool terminal() const {
-    return dot == rule->end();
-  }
+  bool terminal() const { return dot == rule->end(); }
 
   DottedItem next() const {
     DottedItem item;
@@ -72,7 +66,7 @@ public:
 };
 
 class State {
-public:
+ public:
   State() {}
 
   template <typename _ForwardIterator>
@@ -82,16 +76,14 @@ public:
 
   static State &intern(const State &state) {
     StatePtr ptr = std::find(first_state(), last_state(), state);
-    if (ptr == last_state())
-      ptr = states().insert(last_state(), state);
+    if (ptr == last_state()) ptr = states().insert(last_state(), state);
     return *ptr;
   }
 
   State &next(char ch) {
     std::vector<DottedItem> n;
     for (DottedItemPtr it = first_item(); it != last_item(); ++it) {
-      if (! it->terminal() && *it->dot == ch)
-        n.push_back(it->next());
+      if (!it->terminal() && *it->dot == ch) n.push_back(it->next());
     }
     return intern(State(n.begin(), n.end()));
   }
@@ -99,8 +91,7 @@ public:
   std::set<char> firsts() {
     std::set<char> s;
     for (DottedItemPtr it = first_item(); it != last_item(); ++it) {
-      if (! it->terminal())
-        s.insert(*it->dot);
+      if (!it->terminal()) s.insert(*it->dot);
     }
     return s;
   }
@@ -113,8 +104,8 @@ public:
   static StatePtr first_state() { return states().begin(); }
   static StatePtr last_state() { return states().end(); }
 
-  bool operator == (const State &other) const { return _items == other._items; }
-  bool operator != (const State &other) const { return _items != other._items; }
+  bool operator==(const State &other) const { return _items == other._items; }
+  bool operator!=(const State &other) const { return _items != other._items; }
 
   template <typename _Iterator>
   static State &start(_Iterator first, _Iterator last) {
@@ -124,17 +115,15 @@ public:
     return intern(State(items.begin(), items.end()));
   }
 
-  static void reset() {
-    states().clear();
-  }
+  static void reset() { states().clear(); }
 
-private:
+ private:
   static StateList &states() {
     static StateList _states;
     return _states;
   }
 
-private:
+ private:
   std::vector<DottedItem> _items;
 };
 
@@ -145,15 +134,13 @@ static std::string option_token_prefix = "Token_";
 static std::string option_char_type = "char";
 static std::string option_unicode_function = "";
 
-std::string token_id(const std::string &id)
-{
+std::string token_id(const std::string &id) {
   std::string token = option_token_prefix;
 
-  if (! option_toupper)
+  if (!option_toupper)
     token += id;
   else {
-    for (size_t i = 0; i < id.size(); ++i)
-      token += toupper(id[i]);
+    for (size_t i = 0; i < id.size(); ++i) token += toupper(id[i]);
   }
 
   return token;
@@ -166,8 +153,7 @@ bool starts_with(const std::string &line, const std::string &text) {
   return false;
 }
 
-void doit(State &state)
-{
+void doit(State &state) {
   static int depth{0};
 
   ++depth;
@@ -177,23 +163,26 @@ void doit(State &state)
   std::set<char> firsts = state.firsts();
   for (std::set<char>::iterator it = firsts.begin(); it != firsts.end(); ++it) {
     std::string _else = it == firsts.begin() ? "" : "else ";
-    std::cout << indent << _else << "if (s[" << (depth - 1) << "]" << option_unicode_function << " == '" << *it << "') {" << std::endl;
+    std::cout << indent << _else << "if (s[" << (depth - 1) << "]"
+              << option_unicode_function << " == '" << *it << "') {"
+              << std::endl;
     State &next_state = state.next(*it);
 
     bool found = false;
-    for (DottedItemPtr item = next_state.first_item(); item != next_state.last_item(); ++item) {
+    for (DottedItemPtr item = next_state.first_item();
+         item != next_state.last_item(); ++item) {
       if (item->terminal()) {
-	if (found) {
-	  std::cerr << "*** Error. Too many accepting states" << std::endl;
-	  exit(EXIT_FAILURE);
-	}
-	found = true;
-	std::cout << indent << "  return " << option_namespace_name << token_id(*item->rule) << ";" << std::endl;
+        if (found) {
+          std::cerr << "*** Error. Too many accepting states" << std::endl;
+          exit(EXIT_FAILURE);
+        }
+        found = true;
+        std::cout << indent << "  return " << option_namespace_name
+                  << token_id(*item->rule) << ";" << std::endl;
       }
     }
 
-    if (! found)
-      doit(next_state);
+    if (!found) doit(next_state);
 
     std::cout << indent << "}" << std::endl;
   }
@@ -201,43 +190,48 @@ void doit(State &state)
   --depth;
 }
 
-void gen_classify_n(State &start_state, int N)
-{
-  std::cout << "static inline int classify" << N << "(const " << option_char_type << " *s) {" << std::endl;
+void gen_classify_n(State &start_state, int N) {
+  std::cout << "static inline int classify" << N << "(const "
+            << option_char_type << " *s) {" << std::endl;
   doit(start_state);
-  std::cout << "  return " << option_namespace_name << token_id("identifier") << ";" << std::endl
-            << "}" << std::endl << std::endl;
+  std::cout << "  return " << option_namespace_name << token_id("identifier")
+            << ";" << std::endl
+            << "}" << std::endl
+            << std::endl;
 }
 
-void gen_classify(const std::multimap<size_t, std::string> &keywords)
-{
-  std::cout << "static int " << option_namespace_name << "classify(const " << option_char_type << " *s, int n) {" << std::endl
+void gen_classify(const std::multimap<size_t, std::string> &keywords) {
+  std::cout << "static int " << option_namespace_name << "classify(const "
+            << option_char_type << " *s, int n) {" << std::endl
             << "  switch (n) {" << std::endl;
   std::multimap<size_t, std::string>::const_iterator it = keywords.begin();
   while (it != keywords.end()) {
     size_t size = it->first;
-    std::cout << "    case " << size << ": return classify" << size << "(s);" << std::endl;
-    do { ++it; } while (it != keywords.end() && it->first == size);
+    std::cout << "    case " << size << ": return classify" << size << "(s);"
+              << std::endl;
+    do {
+      ++it;
+    } while (it != keywords.end() && it->first == size);
   }
-  std::cout << "    default: return " << option_namespace_name << token_id("identifier") << ";" << std::endl
+  std::cout << "    default: return " << option_namespace_name
+            << token_id("identifier") << ";" << std::endl
             << "  } // switch" << std::endl
-            << "}" << std::endl << std::endl;
+            << "}" << std::endl
+            << std::endl;
 }
 
-void gen_enums(const std::multimap<size_t, std::string> &keywords)
-{
+void gen_enums(const std::multimap<size_t, std::string> &keywords) {
   std::cout << "enum {" << std::endl;
   std::multimap<size_t, std::string>::const_iterator it = keywords.begin();
   for (; it != keywords.end(); ++it) {
     std::cout << "  " << token_id(it->second) << "," << std::endl;
   }
   std::cout << "  " << token_id("identifier") << std::endl
-            << "};" << std::endl << std::endl;
+            << "};" << std::endl
+            << std::endl;
 }
 
-inline bool not_whitespace_p(char ch) {
-  return ! std::isspace(ch);
-}
+inline bool not_whitespace_p(char ch) { return !std::isspace(ch); }
 
 int main(int argc, char *argv[]) {
   const std::string ns = "--namespace=";
@@ -265,65 +259,70 @@ int main(int argc, char *argv[]) {
   const std::string opt_unicode_function = "%unicode-function=";
 
   while (getline(std::cin, textline)) {
-
     // remove trailing spaces
-    textline.assign(textline.begin(), std::find_if(textline.rbegin(), textline.rend(), not_whitespace_p).base());
+    textline.assign(
+        textline.begin(),
+        std::find_if(textline.rbegin(), textline.rend(), not_whitespace_p)
+            .base());
 
-    if (! readKeywords) {
+    if (!readKeywords) {
       if (textline.size() >= 2 && textline[0] == '%') {
-	if (textline[1] == '%') {
-	  readKeywords = true;
-	} else if (textline == opt_no_enums) {
-	  option_no_enums = true;
-	} else if (textline == opt_toupper) {
-	  option_toupper = true;
-	} else if (starts_with(textline, opt_tok_prefix)) {
-	  option_token_prefix.assign(textline.begin() + opt_tok_prefix.size(), textline.end());
-	} else if (starts_with(textline, opt_char_type)) {
-	  option_char_type.assign(textline.begin() + opt_char_type.size(), textline.end());
-	} else if (starts_with(textline, opt_unicode_function)) {
-	  option_unicode_function.assign(textline.begin() + opt_unicode_function.size(), textline.end());
-	} else if (starts_with(textline, opt_ns)) {
-	  option_namespace_name.assign(textline.begin() + opt_ns.size(), textline.end());
-	  option_namespace_name += "::";
-	}
+        if (textline[1] == '%') {
+          readKeywords = true;
+        } else if (textline == opt_no_enums) {
+          option_no_enums = true;
+        } else if (textline == opt_toupper) {
+          option_toupper = true;
+        } else if (starts_with(textline, opt_tok_prefix)) {
+          option_token_prefix.assign(textline.begin() + opt_tok_prefix.size(),
+                                     textline.end());
+        } else if (starts_with(textline, opt_char_type)) {
+          option_char_type.assign(textline.begin() + opt_char_type.size(),
+                                  textline.end());
+        } else if (starts_with(textline, opt_unicode_function)) {
+          option_unicode_function.assign(
+              textline.begin() + opt_unicode_function.size(), textline.end());
+        } else if (starts_with(textline, opt_ns)) {
+          option_namespace_name.assign(textline.begin() + opt_ns.size(),
+                                       textline.end());
+          option_namespace_name += "::";
+        }
 
-	continue;
+        continue;
       }
       std::cout << textline << std::endl;
     } else {
-      if (textline.empty())
-	continue;
+      if (textline.empty()) continue;
 
       std::string::iterator start = textline.begin();
-      while (start != textline.end() && std::isspace(*start))
-	++start;
+      while (start != textline.end() && std::isspace(*start)) ++start;
 
       std::string::iterator stop = start;
       while (stop != textline.end() && (std::isalnum(*stop) || *stop == '_'))
-	++stop;
+        ++stop;
 
       if (start != stop) {
-	std::string keyword(start, stop);
-	if (keyword == "identifier") {
-	  std::cerr << "*** Error. `identifier' is reserved" << std::endl;
-	  exit(EXIT_FAILURE);
-	}
+        std::string keyword(start, stop);
+        if (keyword == "identifier") {
+          std::cerr << "*** Error. `identifier' is reserved" << std::endl;
+          exit(EXIT_FAILURE);
+        }
 
-	keywords.insert(std::make_pair(keyword.size(), keyword));
+        keywords.insert(std::make_pair(keyword.size(), keyword));
       }
     }
   }
 
-  if (! option_no_enums)
-    gen_enums(keywords);
+  if (!option_no_enums) gen_enums(keywords);
 
   std::multimap<size_t, std::string>::iterator it = keywords.begin();
   while (it != keywords.end()) {
     size_t size = it->first;
     RuleList rules;
-    do { rules.push_back(it->second); ++it; }
-    while (it != keywords.end() && it->first == size);
+    do {
+      rules.push_back(it->second);
+      ++it;
+    } while (it != keywords.end() && it->first == size);
     gen_classify_n(State::start(rules.begin(), rules.end()), int(size));
     State::reset();
   }
