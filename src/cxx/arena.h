62bafd4200d7832ed2ce21259d3d02189363fb68
Roberto Raggi <roberto.raggi@gmail.com>
2021-01-08 23:21:36 +0000

    fix signature

--- src/cxx/arena.h
+++ src/cxx/arena.h
@@ -65,7 +65,7 @@ struct Managed {
     return arena->allocate(size);
   }
   void operator delete(void* ptr, std::size_t) {}
-  void operator delete(void* ptr, std::size_t, Arena*) {}
+  void operator delete(void* ptr, std::size_t, Arena*) noexcept {}
 };
 
 }  // namespace cxx
