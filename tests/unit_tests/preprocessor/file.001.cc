ca51296ffc10577446be0de0c8ea1497276980ad
Roberto Raggi <roberto.raggi@gmail.com>
2021-11-08 19:54:03 +0000

    chore(ci): Avoid using platform specific path separators in the unit tests

--- tests/unit_tests/preprocessor/file.001.cc
+++ tests/unit_tests/preprocessor/file.001.cc
@@ -2,12 +2,12 @@
 
 const char* file = __FILE__;
 
-// CHECK: {{^}}const char* file = "{{.*}}/file.001.cc";{{$}}
+// CHECK: {{^}}const char* file = "{{.*}}file.001.cc";{{$}}
 
 #include "file.001.h"
 
-// CHECK: {{^}}const char* file_h = "{{.*}}/file.001.h";{{$}}
+// CHECK: {{^}}const char* file_h = "{{.*}}file.001.h";{{$}}
 
 const char* file_cc = __FILE__;
 
-// CHECK: {{^}}const char* file_cc = "{{.*}}/file.001.cc";{{$}}
+// CHECK: {{^}}const char* file_cc = "{{.*}}file.001.cc";{{$}}
