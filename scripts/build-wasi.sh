4396c1c1dd6be113d6bec831f8cfea6ac12afd5b
Roberto Raggi <roberto.raggi@gmail.com>
2023-06-11 11:33:30 +0200

    chore: Improve WASI CI (#86)

---
+++ scripts/build-wasi.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+me=$(dirname "$0")
+
+project_root=$(cd "$me/.." && pwd)
+
+DOCKER_EXTRA_OPTS="--rm -t -v ${project_root}:/code -w /code -u $(id -u) cxx-wasi"
+
+docker build -t cxx-wasi -f Dockerfile.wasi ${project_root}
+
+docker run ${DOCKER_EXTRA_OPTS} \
+    cmake -G Ninja \
+    -S . \
+    -B build.wasi \
+    -DCMAKE_INSTALL_PREFIX=build.wasi/install/usr \
+    -DCMAKE_TOOLCHAIN_FILE=/usr/share/cmake/wasi-sdk.cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=1 \
+    -DKWGEN_EXECUTABLE=/usr/bin/kwgen \
+    -DCXX_INSTALL_WASI_SYSROOT=ON \
+
+docker run ${DOCKER_EXTRA_OPTS} \
+    cmake --build build.wasi --target install
+
+
