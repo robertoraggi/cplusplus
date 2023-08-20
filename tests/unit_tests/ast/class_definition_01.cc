606acd5d6e71de39c9139671e455e24b8ceab78a
Roberto Raggi <roberto.raggi@gmail.com>
2023-08-20 06:43:24 +0000

    chore: Add test for class definition AST

---
+++ tests/unit_tests/ast/class_definition_01.cc
@@ -0,0 +1,65 @@
+// RUN: %cxx -verify -ast-dump %s | %filecheck %s --match-full-lines
+
+class EmptyClass {};
+
+class OtherEmptyClass {};
+
+class FinalClass final {};
+
+class DerivedClass : public EmptyClass {};
+
+class DerivedClass2 : public EmptyClass, private OtherEmptyClass {};
+
+class DerivedClass3 : virtual public EmptyClass {};
+
+// clang-format off
+//      CHECK:translation-unit
+// CHECK-NEXT:  declaration-list
+// CHECK-NEXT:    simple-declaration
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        class-specifier
+// CHECK-NEXT:          name: simple-name
+// CHECK-NEXT:            identifier: EmptyClass
+// CHECK-NEXT:    simple-declaration
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        class-specifier
+// CHECK-NEXT:          name: simple-name
+// CHECK-NEXT:            identifier: OtherEmptyClass
+// CHECK-NEXT:    simple-declaration
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        class-specifier
+// CHECK-NEXT:          name: simple-name
+// CHECK-NEXT:            identifier: FinalClass
+// CHECK-NEXT:    simple-declaration
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        class-specifier
+// CHECK-NEXT:          name: simple-name
+// CHECK-NEXT:            identifier: DerivedClass
+// CHECK-NEXT:          base-clause: base-clause
+// CHECK-NEXT:            base-specifier-list
+// CHECK-NEXT:              base-specifier
+// CHECK-NEXT:                name: simple-name
+// CHECK-NEXT:                  identifier: EmptyClass
+// CHECK-NEXT:    simple-declaration
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        class-specifier
+// CHECK-NEXT:          name: simple-name
+// CHECK-NEXT:            identifier: DerivedClass2
+// CHECK-NEXT:          base-clause: base-clause
+// CHECK-NEXT:            base-specifier-list
+// CHECK-NEXT:              base-specifier
+// CHECK-NEXT:                name: simple-name
+// CHECK-NEXT:                  identifier: EmptyClass
+// CHECK-NEXT:              base-specifier
+// CHECK-NEXT:                name: simple-name
+// CHECK-NEXT:                  identifier: OtherEmptyClass
+// CHECK-NEXT:    simple-declaration
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        class-specifier
+// CHECK-NEXT:          name: simple-name
+// CHECK-NEXT:            identifier: DerivedClass3
+// CHECK-NEXT:          base-clause: base-clause
+// CHECK-NEXT:            base-specifier-list
+// CHECK-NEXT:              base-specifier
+// CHECK-NEXT:                name: simple-name
+// CHECK-NEXT:                  identifier: EmptyClass
