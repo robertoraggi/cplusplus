8f01dfd55592fbcb94886b93d1179b4b3a2ca77f
Roberto Raggi <roberto.raggi@gmail.com>
2023-09-22 21:20:42 +0200

    fix: Rewrote build script using zx

--- packages/cxx-frontend/scripts/build.js
+++ packages/cxx-frontend/scripts/build.js
@@ -18,32 +18,41 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import "zx/globals";
+
 import * as esbuild from "esbuild";
 import * as path from "path";
-import * as process from "process";
-import { fileURLToPath } from "url";
-import { $, ProcessOutput, chalk } from "zx";
 
-const buildScriptPath = path.dirname(fileURLToPath(import.meta.url));
+// get the root directory of the project
+const projectRootSourcePath = path.join(__dirname, "../../..");
 
-const projectRootSourcePath = path.join(buildScriptPath, "../../..");
-
+// get the source path of the cxx-frontend package
 const cxxFrontendSourcePath = path.join(
   projectRootSourcePath,
   "packages/cxx-frontend",
 );
 
+// set the current working directory to the cxx-frontend source path
 $.cwd = cxxFrontendSourcePath;
 
-async function main() {
-  await $`${projectRootSourcePath}/scripts/build-emscripten.sh`;
+await build();
+
+async function build() {
+  const sdk = await detectEmsdk();
+
+  if (argv.docker === true || !sdk) {
+    await dockerBuild();
+  } else {
+    await emsdkBuild(sdk);
+  }
+
   await $`cp ${projectRootSourcePath}/README.md .`;
   await $`cp ${projectRootSourcePath}/CHANGELOG.md .`;
   await $`cp ${projectRootSourcePath}/build.em/src/js/cxx-js.js src`;
   await $`mkdir -p dist/wasm/ dist/dts/`;
   await $`cp ${projectRootSourcePath}/build.em/src/js/cxx-js.wasm dist/wasm`;
   await $`cp ${cxxFrontendSourcePath}/src/*.d.ts dist/dts/`;
-  await $`npm exec --package-typescript tsc`;
+  await $`npm exec --package typescript tsc`;
 
   /** @type{esbuild.BuildOptions} */
   const buildOptions = {
@@ -53,6 +62,7 @@ async function main() {
     packages: "external",
   };
 
+  // build the cjs cxx-frontend package
   esbuild.build({
     ...buildOptions,
     outfile: "dist/cjs/index.cjs",
@@ -75,11 +85,87 @@ async function main() {
   });
 }
 
-main().catch((err) => {
-  if (err instanceof ProcessOutput) {
-    console.error(err.message);
+async function dockerBuild() {
+  const docker = await which("docker", { nothrow: true });
+  if (!docker) throw new Error("docker not found");
+
+  const emscriptenCacheDir = path.join(os.homedir(), ".emscripten-cache");
+
+  const cmakeOptions = [
+    "-DCMAKE_INSTALL_PREFIX=build.em/install/usr",
+    "-DKWGEN_EXECUTABLE=/usr/bin/kwgen",
+    "-DFLATBUFFERS_FLATC_EXECUTABLE=/usr/bin/flatc",
+    "-S .",
+    "-B build.em",
+  ];
+
+  if (argv.debug) {
+    cmakeOptions.push("-DCMAKE_BUILD_TYPE=Debug");
   } else {
-    console.error(err);
+    cmakeOptions.push(
+      "-DCMAKE_BUILD_TYPE=MinSizeRel",
+      "-DCXX_INTERPROCEDURAL_OPTIMIZATION=ON",
+    );
   }
-  process.exit(1);
-});
+
+  const user = await $`id -u`;
+
+  await $`mkdir -p ${emscriptenCacheDir}`;
+  await $`${docker} build -t cxx-emsdk -f ${projectRootSourcePath}/Dockerfile.emsdk ${projectRootSourcePath}`;
+  await $`${docker} run -t --rm -u ${user} -v ${emscriptenCacheDir}:/emsdk/upstream/emscripten/cache/ cxx-emsdk embuilder.py build MINIMAL --lto=thin`;
+  await $`${docker} run -t --rm -u ${user} -v ${emscriptenCacheDir}:/emsdk/upstream/emscripten/cache/ -v ${projectRootSourcePath}:/code -w /code cxx-emsdk emcmake cmake ${cmakeOptions}`;
+  await $`${docker} run -t --rm -u ${user} -v ${emscriptenCacheDir}:/emsdk/upstream/emscripten/cache/ -v ${projectRootSourcePath}:/code -w /code cxx-emsdk cmake --build build.em`;
+}
+
+async function emsdkBuild({ cmake, emcmake, flatc, kwgen }) {
+  const CMAKE_INTERPROCEDURAL_OPTIMIZATION =
+    $.env.CMAKE_INTERPROCEDURAL_OPTIMIZATION ?? "ON";
+
+  const cmakeOptions = [
+    `-DCMAKE_INSTALL_PREFIX=${projectRootSourcePath}/build.em/install/usr`,
+    `-DKWGEN_EXECUTABLE=${kwgen}`,
+    `-DFLATBUFFERS_FLATC_EXECUTABLE=${flatc}`,
+    `-S ${projectRootSourcePath}`,
+    `-B ${projectRootSourcePath}/build.em`,
+  ];
+
+  if (argv.debug) {
+    cmakeOptions.push(`-DCMAKE_BUILD_TYPE=Debug`);
+  } else {
+    cmakeOptions.push(
+      `-DCMAKE_BUILD_TYPE=MinSizeRel`,
+      `-DCXX_INTERPROCEDURAL_OPTIMIZATION=${CMAKE_INTERPROCEDURAL_OPTIMIZATION}`,
+    );
+  }
+
+  await $`${emcmake} ${cmake} ${cmakeOptions}`;
+
+  await $`${cmake} --build ${projectRootSourcePath}/build.em --target install`;
+}
+
+async function detectEmsdk() {
+  const cmake = await which("cmake", { nothrow: true });
+  if (!cmake) return null;
+
+  const emcmake = await which("emcmake", { nothrow: true });
+  if (!emcmake) return null;
+
+  const flatc =
+    argv.flatc ??
+    $.env.FLATBUFFERS_FLATC_EXECUTABLE ??
+    (await which("flatc", { nothrow: true }));
+  if (!flatc) return null;
+
+  const kwgen =
+    argv.kwgen ??
+    $.env.KWGEN_EXECUTABLE ??
+    (await which("kwgen", { nothrow: true }));
+  if (!kwgen) return null;
+
+  return {
+    cmake,
+    emcmake,
+    flatc,
+    kwgen,
+  };
+}
