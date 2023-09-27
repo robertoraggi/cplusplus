7dfd83f42940a27ce378b2c22b76720a5e6a9d78
Roberto Raggi <roberto.raggi@gmail.com>
2023-09-27 19:50:49 +0200

    fix: Parse of the true branch of an if statement

---
+++ tests/unit_tests/ast/if_statement_01.cc
@@ -0,0 +1,33 @@
+// RUN: %cxx -verify -ast-dump %s | %filecheck %s --match-full-lines
+
+auto main() -> int {
+  if (false)
+
+  // expected-error@1 {{expected a statement}}
+}
+
+// clang-format off
+//      CHECK:translation-unit
+// CHECK-NEXT:  declaration-list
+// CHECK-NEXT:    function-definition
+// CHECK-NEXT:      decl-specifier-list
+// CHECK-NEXT:        auto-type-specifier
+// CHECK-NEXT:      declarator: declarator
+// CHECK-NEXT:        core-declarator: id-declarator
+// CHECK-NEXT:          declarator-id: id-expression
+// CHECK-NEXT:            unqualified-id: name-id
+// CHECK-NEXT:              identifier: main
+// CHECK-NEXT:        declarator-chunk-list
+// CHECK-NEXT:          function-declarator-chunk
+// CHECK-NEXT:            parameters-and-qualifiers: parameters-and-qualifiers
+// CHECK-NEXT:            trailing-return-type: trailing-return-type
+// CHECK-NEXT:              type-id: type-id
+// CHECK-NEXT:                type-specifier-list
+// CHECK-NEXT:                  integral-type-specifier
+// CHECK-NEXT:                    specifier: int
+// CHECK-NEXT:                declarator: declarator
+// CHECK-NEXT:      function-body: compound-statement-function-body
+// CHECK-NEXT:        statement: compound-statement
+// CHECK-NEXT:          statement-list
+// CHECK-NEXT:            if-statement
+// CHECK-NEXT:              condition: bool-literal-expression
