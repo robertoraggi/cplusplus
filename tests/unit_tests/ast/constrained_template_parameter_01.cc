38b965a88fa53460e10e7392af2ebe496c7b2ad8
Roberto Raggi <roberto.raggi@gmail.com>
2023-10-07 13:57:47 +0200

    fix: Parse of constrained template parameters

---
+++ tests/unit_tests/ast/constrained_template_parameter_01.cc
@@ -0,0 +1,97 @@
+// RUN: %cxx -verify -ast-dump %s | %filecheck %s --match-full-lines
+
+template <typename T>
+concept Any = true;
+
+template <Any auto x>
+struct ident {
+  static constexpr auto value = x;
+};
+
+template <Any auto x>
+constexpr auto ident_v = ident<x>::value;// clang-format off
+//      CHECK:translation-unit
+// CHECK-NEXT:  declaration-list
+// CHECK-NEXT:    template-declaration
+// CHECK-NEXT:      template-parameter-list
+// CHECK-NEXT:        typename-type-parameter
+// CHECK-NEXT:          identifier: T
+// CHECK-NEXT:      declaration: concept-definition
+// CHECK-NEXT:        identifier: Any
+// CHECK-NEXT:        expression: bool-literal-expression
+// CHECK-NEXT:          is-true: true
+// CHECK-NEXT:    template-declaration
+// CHECK-NEXT:      template-parameter-list
+// CHECK-NEXT:        parameter-declaration
+// CHECK-NEXT:          type-specifier-list
+// CHECK-NEXT:            placeholder-type-specifier
+// CHECK-NEXT:              type-constraint: type-constraint
+// CHECK-NEXT:                identifier: Any
+// CHECK-NEXT:              specifier: auto-type-specifier
+// CHECK-NEXT:          declarator: declarator
+// CHECK-NEXT:            core-declarator: id-declarator
+// CHECK-NEXT:              declarator-id: id-expression
+// CHECK-NEXT:                unqualified-id: name-id
+// CHECK-NEXT:                  identifier: x
+// CHECK-NEXT:      declaration: simple-declaration
+// CHECK-NEXT:        decl-specifier-list
+// CHECK-NEXT:          class-specifier
+// CHECK-NEXT:            class-key: struct
+// CHECK-NEXT:            unqualified-id: name-id
+// CHECK-NEXT:              identifier: ident
+// CHECK-NEXT:            declaration-list
+// CHECK-NEXT:              simple-declaration
+// CHECK-NEXT:                decl-specifier-list
+// CHECK-NEXT:                  static-specifier
+// CHECK-NEXT:                  constexpr-specifier
+// CHECK-NEXT:                  auto-type-specifier
+// CHECK-NEXT:                init-declarator-list
+// CHECK-NEXT:                  init-declarator
+// CHECK-NEXT:                    declarator: declarator
+// CHECK-NEXT:                      core-declarator: id-declarator
+// CHECK-NEXT:                        declarator-id: id-expression
+// CHECK-NEXT:                          unqualified-id: name-id
+// CHECK-NEXT:                            identifier: value
+// CHECK-NEXT:                    initializer: equal-initializer
+// CHECK-NEXT:                      expression: id-expression
+// CHECK-NEXT:                        unqualified-id: name-id
+// CHECK-NEXT:                          identifier: x
+// CHECK-NEXT:    template-declaration
+// CHECK-NEXT:      template-parameter-list
+// CHECK-NEXT:        parameter-declaration
+// CHECK-NEXT:          type-specifier-list
+// CHECK-NEXT:            placeholder-type-specifier
+// CHECK-NEXT:              type-constraint: type-constraint
+// CHECK-NEXT:                identifier: Any
+// CHECK-NEXT:              specifier: auto-type-specifier
+// CHECK-NEXT:          declarator: declarator
+// CHECK-NEXT:            core-declarator: id-declarator
+// CHECK-NEXT:              declarator-id: id-expression
+// CHECK-NEXT:                unqualified-id: name-id
+// CHECK-NEXT:                  identifier: x
+// CHECK-NEXT:      declaration: simple-declaration
+// CHECK-NEXT:        decl-specifier-list
+// CHECK-NEXT:          constexpr-specifier
+// CHECK-NEXT:          auto-type-specifier
+// CHECK-NEXT:        init-declarator-list
+// CHECK-NEXT:          init-declarator
+// CHECK-NEXT:            declarator: declarator
+// CHECK-NEXT:              core-declarator: id-declarator
+// CHECK-NEXT:                declarator-id: id-expression
+// CHECK-NEXT:                  unqualified-id: name-id
+// CHECK-NEXT:                    identifier: ident_v
+// CHECK-NEXT:            initializer: equal-initializer
+// CHECK-NEXT:              expression: id-expression
+// CHECK-NEXT:                nested-name-specifier: template-nested-name-specifier
+// CHECK-NEXT:                  template-id: simple-template-id
+// CHECK-NEXT:                    identifier: ident
+// CHECK-NEXT:                    template-argument-list
+// CHECK-NEXT:                      type-template-argument
+// CHECK-NEXT:                        type-id: type-id
+// CHECK-NEXT:                          type-specifier-list
+// CHECK-NEXT:                            named-type-specifier
+// CHECK-NEXT:                              unqualified-id: name-id
+// CHECK-NEXT:                                identifier: x
+// CHECK-NEXT:                          declarator: declarator
+// CHECK-NEXT:                unqualified-id: name-id
+// CHECK-NEXT:                  identifier: value
