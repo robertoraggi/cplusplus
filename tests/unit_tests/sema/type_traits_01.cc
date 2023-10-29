2d5970c6a326c8d0707bfc7780a64ec23a5fac38
Roberto Raggi <roberto.raggi@gmail.com>
2023-10-29 11:41:51 +0100

    fix: Implemented __is_lvalue_reference and __is_rvalue_reference type traits

--- tests/unit_tests/sema/type_traits_01.cc
+++ tests/unit_tests/sema/type_traits_01.cc
@@ -42,3 +42,35 @@ static_assert(__is_volatile(volatile void*));
 
 // expected-error@1 {{static assert failed}}
 static_assert(__is_volatile(int));
+
+//
+// is_lvalue_reference trait
+//
+
+static_assert(__is_lvalue_reference(int&));
+static_assert(__is_lvalue_reference(const int&));
+
+// expected-error@1 {{static assert failed}}
+static_assert(__is_lvalue_reference(int&&));
+
+//
+// is_rvalue_reference trait
+//
+
+static_assert(__is_rvalue_reference(int&&));
+static_assert(__is_rvalue_reference(const int&&));
+
+// expected-error@1 {{static assert failed}}
+static_assert(__is_rvalue_reference(int&));
+
+//
+// is_reference trait
+//
+
+static_assert(__is_reference(int&));
+static_assert(__is_reference(const int&));
+static_assert(__is_reference(int&&));
+static_assert(__is_reference(const int&&));
+
+// expected-error@1 {{static assert failed}}
+static_assert(__is_reference(int));
