8d3d2a2e08c89461ab7137790c18113e0ac930b2
Roberto Raggi <roberto.raggi@gmail.com>
2024-11-03 20:37:06 +0000

    chore: Support C++26 toolchain on Linux

--- src/parser/cxx/gcc_linux_toolchain.h
+++ src/parser/cxx/gcc_linux_toolchain.h
@@ -38,6 +38,11 @@ class GCCLinuxToolchain final : public Toolchain {
   void addSystemCppIncludePaths() override;
   void addPredefinedMacros() override;
 
+  void addCommonMacros();
+  void addAmd64Macros();
+  void addArm64Macros();
+  void addCxx26Macros();
+
  private:
   std::optional<int> version_;
   std::string arch_;
