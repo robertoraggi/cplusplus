83071e1bfc03bafaad1ed8a66b2cec7fbd3dcf56
Roberto Raggi <roberto.raggi@gmail.com>
2024-12-14 14:09:59 +0000

    chore: Update WASI SDK to version 25
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- .devcontainer/features/wasi-sdk/install.sh
+++ .devcontainer/features/wasi-sdk/install.sh
@@ -8,6 +8,6 @@ if [ "$arch" == "aarch64" ]; then
     arch="arm64"
 fi
 
-wget -nd -P /tmp/ https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-24/wasi-sdk-24.0-${arch}-linux.deb
+wget -nd -P /tmp/ https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-25/wasi-sdk-25.0-${arch}-linux.deb
 
-dpkg -i /tmp/wasi-sdk-24.0-${arch}-linux.deb
+dpkg -i /tmp/wasi-sdk-25.0-${arch}-linux.deb
