e4a0719abe86fcf23fdf40aab9bc71111905bfc7
Roberto Raggi <roberto.raggi@gmail.com>
2024-12-27 15:32:22 +0100

    feat: Improve parsing of template introduced name ids
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- tests/manual/source.cc
+++ tests/manual/source.cc
@@ -5,40 +5,44 @@
 #include <any>
 #include <array>
 #include <atomic>
+#ifdef __cpp_lib_barrier
+#include <barrier>
+#endif
 #include <bit>
 #include <bitset>
-#include <cassert>
-#include <ccomplex>
-#include <cctype>
-#include <cerrno>
-#include <cfenv>
-#include <cfloat>
 #include <charconv>
 #include <chrono>
-#include <cinttypes>
-#include <ciso646>
-#include <climits>
-#include <clocale>
-#include <cmath>
 #include <codecvt>
+#include <compare>
 #include <complex>
+#include <concepts>
 #include <condition_variable>
-#include <cstdarg>
-#include <cstdbool>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctgmath>
-#include <ctime>
-#include <cwchar>
-#include <cwctype>
+#include <coroutine>
 #include <deque>
 #include <exception>
+#include <execution>
+#include <expected>
+
+#ifdef __cpp_lib_filesystem
+#include <filesystem>
+#endif
+
+#ifdef __cpp_lib_flat_map
+#include <flat_map>
+#endif
+
+#ifdef __cpp_lib_flat_set
+#include <flat_set>
+#endif
+#include <format>
 #include <forward_list>
 #include <fstream>
 #include <functional>
+
+#ifdef __cpp_lib_generator
+#include <generator>
+#endif
+
 #include <initializer_list>
 #include <iomanip>
 #include <ios>
@@ -46,30 +50,71 @@
 #include <iostream>
 #include <istream>
 #include <iterator>
+
+#ifdef __cpp_lib_latch
+#include <latch>
+#endif
+
 #include <limits>
 #include <list>
 #include <locale>
 #include <map>
+#include <mdspan>
 #include <memory>
+#include <memory_resource>
+
+#ifdef __cpp_lib_scoped_lock
 #include <mutex>
+#endif
+
 #include <new>
+#include <numbers>
 #include <numeric>
 #include <optional>
 #include <ostream>
+
+#ifdef __cpp_lib_print
+#include <print>
+#endif
+
 #include <queue>
 #include <random>
+#include <ranges>
 #include <ratio>
 #include <regex>
 #include <scoped_allocator>
 #include <set>
+#ifdef __cpp_lib_shared_mutex
+#include <shared_mutex>
+#endif
+#include <source_location>
 #include <span>
+#ifdef __cpp_lib_spanstream
+#include <spanstream>
+#endif
 #include <sstream>
 #include <stack>
+#ifdef __cpp_lib_stacktrace
+#include <stacktrace>
+#endif
 #include <stdexcept>
+#if false
+#include <stdfloat>
+#endif
+#ifdef __cpp_lib_jthread
+#include <stop_token>
+#endif
 #include <streambuf>
 #include <string>
 #include <string_view>
+#include <strstream>
+#include <syncstream>
 #include <system_error>
+#ifndef _LIBCPP_HAS_NO_THREADS
+#include <future>
+#include <semaphore>
+#include <thread>
+#endif
 #include <tuple>
 #include <type_traits>
 #include <typeindex>
@@ -80,18 +125,3 @@
 #include <valarray>
 #include <variant>
 #include <vector>
-
-#ifndef _LIBCPP_HAS_NO_THREADS
-#include <future>
-#include <shared_mutex>
-#include <thread>
-#endif
-
-#ifndef __wasi__
-#include <csetjmp>
-#include <csignal>
-#include <filesystem>
-#endif
-
-// #include <execution>
-// #include <compare>
