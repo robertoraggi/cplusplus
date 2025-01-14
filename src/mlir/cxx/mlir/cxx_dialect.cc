78c0fb62d02c870a155230125c9b2dc8a5dd1b65
Roberto Raggi <roberto.raggi@gmail.com>
2025-01-14 19:20:08 +0100

    feat: Add SCF dialect and generate IR for a few basic expressions and statements
    Also, add the option `-emit-ir` to the CLI to
    generate the IR to the standard output.
    
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- src/mlir/cxx/mlir/cxx_dialect.cc
+++ src/mlir/cxx/mlir/cxx_dialect.cc
@@ -24,6 +24,7 @@
 #include <llvm/ADT/TypeSwitch.h>
 #include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/SCF/IR/SCF.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/IR/DialectImplementation.h>
 
