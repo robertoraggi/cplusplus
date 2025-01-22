18f986ab6ee8118edba40266b8be65969b522221
Roberto Raggi <roberto.raggi@gmail.com>
2025-01-22 18:48:43 +0100

    chore: Update npm deps
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- packages/cxx-storybook/.storybook/main.ts
+++ packages/cxx-storybook/.storybook/main.ts
@@ -4,18 +4,15 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {},
 };
-export default config;
 
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
-}
+export default config;
