611d33dd04ad28bda87123870d20a62b2324e76b
Roberto Raggi <roberto.raggi@gmail.com>
2025-04-26 09:21:36 +0000

    Add APis to compute the qualification combined type
    Signed-off-by: GitHub <noreply@github.com>

--- tests/unit_tests/sema/conditional_expr_01.cc
+++ tests/unit_tests/sema/conditional_expr_01.cc
@@ -1,5 +1,8 @@
 // RUN: %cxx -verify -fcheck %s
 
+struct Base {};
+struct Derived : Base {};
+
 void returns_void();
 int returns_int();
 int& returns_int_ref();
@@ -44,8 +47,40 @@ void test_conditional(bool x) {
   static_assert(__is_same(unsigned, decltype(x ? 0 : 1u)));
   static_assert(__is_same(long, decltype(x ? 0 : 1l)));
 
+  const char cs[] = "";
   char s[10];
-  char* cp;
+  char* p;
+  const char* cp = p;
+  const char* const cpc = p;
 
-  static_assert(__is_same(char*, decltype(x ? s : cp)));
+  static_assert(__is_same(char*, decltype(x ? s : p)));
+
+  static_assert(__is_same(const char*, decltype(true ? p : cp)));
+
+  static_assert(__is_same(const char*, decltype(true ? p : "")));
+  static_assert(__is_same(const char*, decltype(true ? "" : cp)));
+
+  static_assert(__is_same(const char*, decltype(x ? cs : cp)));
+  static_assert(__is_same(const char*, decltype(x ? cs : p)));
+  static_assert(__is_same(const char*, decltype(x ? cs : cpc)));
+
+  Base* b;
+  Derived* d;
+
+  static_assert(__is_same(Base*, decltype(true ? b : d)));
+
+  char (*a10)[10];
+  char (*a100)[100];
+  char (*a)[];
+  const char (*ca)[];
+
+  // clang-format off
+  // expected-error@1 {{left operand to ? is 'char (*)[10]', but right operand is of type 'char (*)[100]'}}
+  x ? a10 : a100;
+  // clang-format on
+
+  static_assert(__is_same(char (*&)[10], decltype(x ? a10 : a10)));
+  static_assert(__is_same(const char (*)[], decltype(x ? a10 : a)));
+  static_assert(__is_same(char (*&)[], decltype(x ? a : a)));
+  static_assert(__is_same(const char (*)[], decltype(x ? a : ca)));
 }
