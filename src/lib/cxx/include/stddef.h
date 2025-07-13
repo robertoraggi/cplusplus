b824e83cbe6a2ec507de7721269c7376f3f190dd
Roberto Raggi <roberto.raggi@gmail.com>
2025-07-13 22:47:54 +0200

    Fix type definitions of size_t and wchar_t in the built-in stddef.h

--- src/lib/cxx/include/stddef.h
+++ src/lib/cxx/include/stddef.h
@@ -1,36 +1,76 @@
-#if !defined(__need_ptrdiff_t) && !defined(__need_size_t) && \
-    !defined(__need_wchar_t) && !defined(__need_NULL) &&     \
-    !defined(__need_STDDEF_H_misc)
+#if !defined(__need_ptrdiff_t) && !defined(__need_size_t) &&     \
+    !defined(__need_wchar_t) && !defined(__need_NULL) &&         \
+    !defined(__need_rsize_t) && !defined(__need_nullptr_t) &&    \
+    !defined(__need_max_align_t) && !defined(__need_offsetof) && \
+    !defined(__need_wint_t)
 #define __need_ptrdiff_t
 #define __need_size_t
 #define __need_wchar_t
-#define __need_NULL
-#define __need_STDDEF_H_misc
+#define __need_offsetof
+#define __need_max_align_t
+
+#if defined(__STDC_WANT_LIB_EXT1__) && __STDC_WANT_LIB_EXT1__ >= 1
+#define __need_rsize_t
 #endif
+
+#if !defined(__STDDEF_H)
+#define __need_NULL
+#endif
+
+#endif /* no needs */
 
 #ifdef __need_ptrdiff_t
 #undef __need_ptrdiff_t
-typedef long int ptrdiff_t;
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
 #endif /* __need_ptrdiff_t */
 
 #ifdef __need_size_t
 #undef __need_size_t
-typedef long unsigned int size_t;
+typedef __SIZE_TYPE__ size_t;
 #endif /* __need_size_t */
+
+#ifdef __need_rsize_t
+#undef __need_rsize_t
+typedef __SIZE_TYPE__ rsize_t;
+#endif /* __need_size_t */
+
+#ifdef __need_ptrdiff_t
+#undef __need_ptrdiff_t
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#endif /* __need_ptrdiff_t */
 
 #ifdef __need_wchar_t
 #undef __need_wchar_t
+#ifndef __cplusplus
+#if !defined(_WCHAR_T)
+#define _WCHAR_T
+typedef __WCHAR_TYPE__ wchar_t;
+#endif /* _WCHAR_T */
+#endif /* __cplusplus */
 #endif /* __need_wchar_t */
 
 #ifdef __need_NULL
 #undef __need_NULL
-#undef NULL
+#if !defined(NULL)
 #define NULL 0
+#endif /* NULL */
 #endif /* __need_NULL */
 
-#ifdef __need_STDDEF_H_misc
-#undef __need_STDDEF_H_misc
-typedef long unsigned int rsize_t;
+#ifdef __need_wint_t
+#undef __need_wint_t
+typedef __WINT_TYPE__ wint_t;
+#endif /* __need_wint_t */
+
+#ifdef __need_offsetof
+#undef __need_offsetof
+#if !defined(offsetof)
+#define offsetof(t, d) __builtin_offsetof(t, d)
+#endif
+
+#endif /* __need_offsetof */
+
+#ifdef __need_max_align_t
+#undef __need_max_align_t
 
 typedef struct {
   long long __clang_max_align_nonce1
@@ -40,19 +80,4 @@ typedef struct {
       __attribute__((__aligned__(__alignof__(long double))));
 } max_align_t;
 
-#endif /* __need_STDDEF_H_misc */
-
-#ifdef __need_wint_t
-#undef __need_wint_t
-
-#ifdef __WINT_TYPE__
-typedef __WINT_TYPE__ wint_t;
-#else
-typedef int wint_t;
-#endif
-
-#endif /* __need_wint_t */
-
-#if !defined(offsetof)
-#define offsetof(t, d) __builtin_offsetof(t, d)
-#endif
+#endif /* __need_max_align_t */
