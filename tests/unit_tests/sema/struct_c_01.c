c394446c46a580be64ddf485c13a911eee65234f
Roberto Raggi <roberto.raggi@gmail.com>
2025-07-19 16:34:39 +0200

    Print enumerator values
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- tests/unit_tests/sema/struct_c_01.c
+++ tests/unit_tests/sema/struct_c_01.c
@@ -26,14 +26,14 @@ int main() {
 // clang-format off
 //      CHECK:namespace
 // CHECK-NEXT:  enum E : int
-// CHECK-NEXT:  enumerator E v
+// CHECK-NEXT:  enumerator E v = 0
 // CHECK-NEXT:  class A
 // CHECK-NEXT:    field E e
 // CHECK-NEXT:    field int x
 // CHECK-NEXT:  function int main()
 // CHECK-NEXT:    block
 // CHECK-NEXT:      enum K : int
-// CHECK-NEXT:      enumerator K w
+// CHECK-NEXT:      enumerator K w = 0
 // CHECK-NEXT:      class B
 // CHECK-NEXT:        field K k
 // CHECK-NEXT:        field E e
