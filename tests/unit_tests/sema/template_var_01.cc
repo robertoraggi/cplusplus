ed5f487a3a3639d775dbdf0d1e89306bbabd8ecd
Roberto Raggi <roberto.raggi@gmail.com>
2025-08-28 18:14:31 +0200

    Dump variable template specializations
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- tests/unit_tests/sema/template_var_01.cc
+++ tests/unit_tests/sema/template_var_01.cc
@@ -6,3 +6,12 @@ constexpr bool is_integral_v = __is_integral(T);
 static_assert(is_integral_v<int>);
 static_assert(is_integral_v<char>);
 static_assert(is_integral_v<void*> == false);
+
+// clang-format off
+//      CHECK:namespace
+// CHECK-NEXT:  template variable constexpr const bool is_integral_v
+// CHECK-NEXT:    parameter typename<0, 0> T
+// CHECK-NEXT:    [specializations]
+// CHECK-NEXT:      variable constexpr bool is_integral_v<int>
+// CHECK-NEXT:      variable constexpr bool is_integral_v<char>
+// CHECK-NEXT:      variable constexpr bool is_integral_v<void*>
