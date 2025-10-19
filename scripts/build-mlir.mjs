04d9836ef1311cd334856a89fe8df6ac3d3ed559
Roberto Raggi <roberto.raggi@gmail.com>
2025-10-19 16:47:31 +0200

    Update npm, mlir and emscripten dependencies
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- scripts/build-mlir.mjs
+++ scripts/build-mlir.mjs
@@ -51,7 +51,7 @@ async function downloadLLVM({ packages, version, outdir }) {
 }
 
 async function main() {
-  const version = "20.1.8";
+  const version = "21.1.3";
   const packages = ["cmake", "third-party", "llvm", "mlir"];
 
   const llvm_source_dir = zx.path.resolve(
