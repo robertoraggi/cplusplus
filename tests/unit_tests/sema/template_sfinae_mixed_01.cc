a90051f15ede0f08c6af296ebe290759b01d9a52
Roberto Raggi <roberto.raggi@gmail.com>
2026-03-18 21:20:47 +0100

    Improve lookup of template function specializations
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

---
+++ tests/unit_tests/sema/template_sfinae_mixed_01.cc
@@ -0,0 +1,29 @@
+// RUN: %cxx -verify -fcheck %s
+// expected-no-diagnostics
+
+template <bool B, class T = void>
+struct enable_if {};
+
+template <class T>
+struct enable_if<true, T> {
+  typedef T type;
+};
+
+template <class T>
+typename enable_if<sizeof(T) == 4, int>::type foo() {
+  return 10;
+}
+
+template <class T>
+typename enable_if<sizeof(T) == 8, int>::type foo() {
+  return 20;
+}
+
+int foo(int x) { return x; }
+
+int main() {
+  int a = foo<int>();
+  int b = foo<long long>();
+  int c = foo(42);
+  return (a == 10 && b == 20 && c == 42) ? 0 : 1;
+}
