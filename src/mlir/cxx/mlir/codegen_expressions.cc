2b4e8b79631c349ab02ae42bdc3424a4a73d67ad
Roberto Raggi <roberto.raggi@gmail.com>
2026-03-25 06:02:39 +0100

    Simplify label and goto resolutions
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

--- src/mlir/cxx/mlir/codegen_expressions.cc
+++ src/mlir/cxx/mlir/codegen_expressions.cc
@@ -1902,8 +1902,7 @@ auto Codegen::ExpressionVisitor::operator()(LabelAddressExpressionAST* ast)
   auto loc = gen.getLocation(ast->firstSourceLocation());
   auto ptrType = mlir::cast<mlir::cxx::PointerType>(gen.convertType(ast->type));
   auto op = mlir::cxx::LabelAddressOp::create(
-      gen.builder_, loc, ptrType, ast->identifier->name(), mlir::IntegerAttr{},
-      mlir::StringAttr{});
+      gen.builder_, loc, ptrType, ast->identifier->name(), mlir::IntegerAttr{});
   return {op};
 }
 
