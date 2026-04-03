54fb756f104a64c9ecb3ad70f58e91cffb913470
Roberto Raggi <roberto.raggi@gmail.com>
2026-04-03 15:51:48 +0200

    Add MemCpyOp to the cxx dialect
    Signed-off-by: Roberto Raggi <roberto.raggi@gmail.com>

---
+++ tests/unit_tests/sema/aggregate_copy_init_01.c
@@ -0,0 +1,13 @@
+// RUN: %cxx -verify -fcheck %s
+// expected-no-diagnostics
+
+struct Point {
+  int x;
+  int y;
+};
+
+int main(void) {
+  struct Point p = {1, 2};
+  struct Point q = p;
+  return q.x + q.y - 3;
+}
