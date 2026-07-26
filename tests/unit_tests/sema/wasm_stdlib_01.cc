ca1d93100ce436ab4b86b34ea9a4d6ac9f707619
Roberto Raggi <roberto.raggi@gmail.com>
2026-07-26 20:40:57 +0200

    Test the WASI stdlib that we can typecheck in c++14 mode

--- tests/unit_tests/sema/wasm_stdlib_01.cc
+++ tests/unit_tests/sema/wasm_stdlib_01.cc
@@ -11,6 +11,7 @@
 #include <climits>
 #include <clocale>
 #include <cmath>
+#include <compare>
 #include <cstdalign>
 #include <cstdarg>
 #include <cstdbool>
@@ -25,14 +26,14 @@
 #include <cwctype>
 #include <initializer_list>
 #include <limits>
+#include <new>
 #include <type_traits>
+#include <typeindex>
+#include <typeinfo>
 
 // #include <csignal>
 // #include <ccomplex>
 // #include <ctgmath>
 
-// #include <memory>
-// #include <new>
 // #include <numbers>
-// #include <typeindex>
-// #include <typeinfo>
+// #include <memory>
