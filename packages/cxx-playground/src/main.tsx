6a93f31b6a9e529d6f9301e1bb282cb1f9e84a79
Roberto Raggi <roberto.raggi@gmail.com>
2026-08-04 20:36:54 +0200

    Fix build of the cxx-playground

--- packages/cxx-playground/src/main.tsx
+++ packages/cxx-playground/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import "./monaco-environment"
 import "./index.css"
 import App from "./App.tsx"
 import { PlaygroundProvider } from "./playground-context.tsx"
